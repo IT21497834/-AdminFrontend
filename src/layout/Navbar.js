91ea4f0b00caabb773662fcdbcc9b24c1f4b1f43
IT21497834 <it21497834@my.sliit.lk>
2023-04-25 18:21:08 +0530

    1st commit

---
+++ src/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <div>
+
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to='/'>Hotel Fresco</Link>
+
+    <button 
+
+        className="navbar-toggler" 
+        type="button"
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation"
+     >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+     
+    <Link className='btn btn-outline-light' to="/adduser">
+        Add Customer
+    </Link>
+  </div>
+</nav>
+
+
+
+        </div>
+    )
+}
+
+export default Navbar
